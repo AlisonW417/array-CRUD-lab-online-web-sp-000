0792b0be34004a17545d9b2808b20298bdfae2fa
Alison Williams <apw5011@gmail.com>
2020-11-06 00:11:30 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,7 +12,8 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array = ["pink", "yellow", "green", "purple"]
+  array.unshift("orange")
 end
 
 def remove_element_from_end_of_array(array)
